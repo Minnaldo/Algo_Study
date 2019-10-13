25218b99f89452bc8255db061bdc573d4097c33c
Minnaldo <minnaldo6602@gmail.com>
2019-10-14 02:05:13 +0900

    BFS와_DFS_인접행렬_인접리스트

--- Min/SWEA/통역사성경이.java
+++ Min/SWEA/통역사성경이.java
@@ -5,28 +5,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
-public class 지그재그숫자 {
+public class 통역사성경이 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			int sum = 0;
-			for(int i = 1; i <= N; i++) {
-				if(i % 2 == 1)
-					sum += i;
-				else
-					sum += -(i);
-			}
 			
-			bw.write("#"+tc+" "+sum+"\n");
-			bw.flush();
+			
 		}
-		
 	}
 
 }
